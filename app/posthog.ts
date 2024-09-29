b8593a6828e85d845e30671ae47f7a19680c776b
Mark Centoni <marklc44@gmail.com>
2024-09-28 17:27:57 -0700

    added posthog node comment

--- app/posthog.ts
+++ app/posthog.ts
@@ -1,5 +1,8 @@
 import { PostHog } from "posthog-node"
 
+/**
+ * Posthog node for interacting with posthog during server rendering
+ */
 export default function PostHogClient() {
   const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY || ''
   const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com'
